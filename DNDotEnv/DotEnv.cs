247eed54f78401e296d9ca50962c4e30d069403b
Fahad Al-Hantushi <abojaber@programmer.net>
2023-09-25 13:32:15 +0300

    init project

---
+++ DNDotEnv/DotEnv.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="ConfigDotEnv.cs" company="thiqah.sa">
+// Copyright (c) thiqah.sa. All rights reserved.
+// </copyright>
+
+namespace DNDotEnv
+{
+    public static class DotEnv
+    {
+        public static void Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception(string.Format(
+                    ".env file is not found in path:" + filePath));
+            }
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length > 2)
+                {
+                    var resultList = parts.ToList();
+                    resultList.Remove(parts[0]);
+
+                    var result = string.Join("=", resultList);
+                    Environment.SetEnvironmentVariable(
+                        parts[0],
+                        result);
+                    continue;
+                }
+
+                // TODO: check the cases
+                if (parts.Length != 2)
+                {
+                    continue;
+                }
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DNDotEnv
+
+This poroject aim to load .env file using minimum footprint.
+
+## Installation
+
+
+## Using the system
